262742b52d7c0835dfe44dda9784a1461f4062f9
Kevin Boonstra <23527353+Strazz1337@users.noreply.github.com>
2024-03-29 14:07:18 +0100

    refactor(logging): moved constants to separate file

--- packages/open-telemetry-nest/src/logging/adapters/nest-winston-logger.adapter.ts
+++ packages/open-telemetry-nest/src/logging/adapters/nest-winston-logger.adapter.ts
@@ -6,28 +6,8 @@ import { GlobalProviders } from '@zonneplan/open-telemetry-node';
 import { LogAttributes, SeverityNumber } from '@opentelemetry/api-logs';
 import { LoggerService } from '../services/logger.service';
 import { context } from '@opentelemetry/api';
+import { NEST_LOG_LEVEL_WINSTON_SEVERITY, SEVERITY_TEXT_TO_NEST_LOG_LEVEL } from '../constants';
 
-/**
- * @see https://github.com/winstonjs/winston?tab=readme-ov-file#logging
- */
-const NEST_LOG_LEVEL_WINSTON_SEVERITY: Record<LogLevel, number> = {
-  fatal: 0,
-  error: 0,
-  warn: 1,
-  log: 2,
-  verbose: 4,
-  debug: 5
-};
-
-const SEVERITY_TEXT_TO_NEST_LOG_LEVEL: Record<string, LogLevel> = {
-  FATAL: 'fatal',
-  ERROR: 'error',
-  WARN: 'warn',
-  INFO: 'log',
-  DEBUG: 'debug',
-  TRACE: 'verbose',
-  UNSPECIFIED: 'log'
-};
 
 export class NestWinstonLoggerAdapter extends LoggerService {
   private severityNumberToSeverityTextMap: Record<number, string> = {
@@ -127,7 +107,7 @@ export class NestWinstonLoggerAdapter extends LoggerService {
 
     const winstonLogLevel = this.toWinstonLogLevel(nestLogLevel);
     this.logger[winstonLogLevel](body, attributes);
-    
+
     if (!GlobalProviders.logProvider) {
       console.error('OpenTelemetry log provider not initialized');
       return;
