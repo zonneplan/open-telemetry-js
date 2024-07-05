5ee76ef1242b83af15210e292ccb12ebceb434f3
Kevin <23527353+Strazz1337@users.noreply.github.com>
2024-07-05 14:11:59 +0200

    chore(ci): sync versions between packages (#23)

---
+++ libs/dependency-syncer/README.md
@@ -0,0 +1,11 @@
+# dependency-syncer
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build dependency-syncer` to build the library.
+
+## Running unit tests
+
+Run `nx test dependency-syncer` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/dependency-syncer/src/generators/sync/generator.ts
@@ -0,0 +1,54 @@
+import { Tree, updateJson } from '@nx/devkit';
+import { getPackageJson } from '@nx/eslint-plugin/src/utils/package-json-utils';
+
+type PackageName = 'open-telemetry-nest' | 'open-telemetry-zonneplan' | 'open-telemetry-node';
+
+// x -> y means y depends on x
+const PackageDependencyMap: Record<PackageName, PackageName[]> = {
+  'open-telemetry-nest': ['open-telemetry-zonneplan'],
+  'open-telemetry-zonneplan': [],
+  'open-telemetry-node': ['open-telemetry-nest', 'open-telemetry-zonneplan']
+};
+
+function getPackagePath(packageName: PackageName) {
+  return `packages/${packageName}/package.json`;
+}
+
+function getPackageIdentifier(packageName: PackageName) {
+  return `@zonneplan/${packageName}`;
+}
+
+export async function syncGenerator(tree: Tree) {
+  Object.keys(PackageDependencyMap).forEach((packageName) => syncDepdendencyForPackage(tree, packageName as PackageName));
+}
+
+function syncDepdendencyForPackage(tree: Tree, packageName: PackageName) {
+  const dependantPackages = PackageDependencyMap[packageName];
+
+  if (!dependantPackages.length) {
+    console.log(`No dependant packages found for ${packageName}`);
+    return;
+  }
+
+
+  const packageIdentifier = getPackageIdentifier(packageName);
+  const currentVersion = getPackageJson(getPackagePath(packageName)).version;
+
+  if (!currentVersion) {
+    console.error(`Could not find version for ${packageIdentifier}`);
+    return;
+  }
+
+  console.log(`Updating ${packageIdentifier} to ${currentVersion}`);
+  for (const dependantPackage of dependantPackages) {
+    updateJson(tree, getPackagePath(dependantPackage), (json) => {
+      json.dependencies ??= {};
+      json.dependencies[packageIdentifier] = `^${currentVersion}`;
+
+      return json;
+    });
+
+  }
+}
+
+export default syncGenerator;
