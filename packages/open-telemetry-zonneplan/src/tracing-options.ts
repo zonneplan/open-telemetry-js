66ee994d1e2d44cac1621ad6b1f0f5319d2b4ca3
Kevin Boonstra <23527353+Strazz1337@users.noreply.github.com>
2025-04-02 08:26:54 +0200

    feat(open-telemetry-zonneplan): enable io-redis tracing

--- packages/open-telemetry-zonneplan/src/tracing-options.ts
+++ packages/open-telemetry-zonneplan/src/tracing-options.ts
@@ -13,9 +13,6 @@ export const DefaultTracingOptions = new OpenTelemetryTracingOptionsBuilder()
       '@opentelemetry/instrumentation-fs': {
         enabled: false
       },
-      '@opentelemetry/instrumentation-ioredis': {
-        enabled: false
-      }
     }),
     new WinstonInstrumentation({
       enabled: true
